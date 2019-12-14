5cd5f55f351e094ae0f56f94476a452050a2055a
b.e.doron <b.e.doron@gmail.com>
2019-12-14 23:37:23 +0200

    Added wifi app installation

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -5,8 +5,9 @@ rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
 on_chroot << EOF
 mkdir -p ~/.ssh/
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-cd /tmp
-git clone git@github.com:jasbur/RaspiWiFi.git
-cd /tmp/RaspiWifi
-python3 initial_setup.py
+cd ~/ && [ ! -d "RaspiWiFi" ] && git clone https://github.com/jasbur/RaspiWiFi.git
+cd ~/RaspiWiFi && \
+    python3 -c 'import setup_lib; setup_lib.install_prereqs()' && \
+    python3 -c 'import setup_lib; setup_lib.copy_configs("N")' && \
+    python3 -c 'import setup_lib; setup_lib.update_main_config_file("", "y", "30", "N", "80", "N", "N")'
 EOF
