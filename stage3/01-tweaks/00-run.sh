9687edd87598806134913e4040c525703535025c
b.e.doron <b.e.doron@gmail.com>
2019-12-14 20:32:16 +0200

    Changed to fit hackathon

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -e
 
 rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
+
+on_chroot << EOF
+mkdir -p ~/.ssh/
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+cd /tmp
+git clone git@github.com:jasbur/RaspiWiFi.git
+cd /tmp/RaspiWifi
+python3 initial_setup.py
+EOF
